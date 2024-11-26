840546f97b7bb5cb5f4e9beba5ad4e226b6ff9ed
teshager habtie <teshie00001@gmail.com>
2024-11-26 10:28:39 -0600

    this is adding contact in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
-import React from 'react'
-import About from './components/About/About'
-import Navbar from './components/Navbar/Navbar'
+import React from "react";
+import About from "./components/About/About";
+import Navbar from "./components/Navbar/Navbar";
 import styles from "./App.module.css";
-
+import Hero from "./components/Hero/Hero";
+import Experience from "./components/Experience/Experience";
+import Projects from "./components/Projects/Projects";
+import Contact from "./components/Contact/Contact.jsx";
 function App() {
   return (
     <div className={styles.App}>
       <Navbar />
+      <Hero />
       <About />
+      <Experience />
+      <Projects />
+      <Contact />
     </div>
   );
 }
 
-export default App
+export default App;
