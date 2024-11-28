7f369523e5c7478900c3d257e1d359fed9252bbb
teshager habtie <teshie00001@gmail.com>
2024-11-28 11:08:31 -0600

    creating loop content

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,11 +2,33 @@ import React, { useState } from "react";
 import styles from "./Hero.module.css";
 import { getImageUrl } from "../../utils.js";
 
+import TextLoop from "react-text-loop";
+import Typewriter from "typewriter-effect";
+const Bio = {
+  roles: ["Developer", "Designer", "Creator", "Engineer"],
+};
+
 function Hero() {
   return (
     <section className={styles.container}>
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm teshager</h1>
+
+        <h1 className={styles.Loops}>
+          I am{" "}
+          <TextLoop>
+            <span>
+              <Typewriter
+                options={{
+                  strings: Bio.roles,
+                  autoStart: true,
+                  loop: true,
+                }}
+              />
+            </span>
+          </TextLoop>{" "}
+        </h1>
+
         <p className={styles.description}>
           I'm a full-stack developer with experience using React and NodeJS.
           Reach out if you'd like to learn more!
