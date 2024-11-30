1e4c6c58056096291b2be740a313495a67839d42
teshager habtie <teshie00001@gmail.com>
2024-11-30 09:34:23 -0600

    this is uppdating in components

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,46 +1,47 @@
-import React, { useState } from 'react'
-import styles from './Navbar.module.css' 
-import '../../index.css'
-import { getImageUrl } from '../../utils.js';
-function Navbar() {
-   const [menuOpen, setMenuOpen] = useState(false);
+import React, { useState } from "react";
+import styles from "./Navbar.module.css";
 
-   return (
-     <nav className={styles.navbar}>
-       <a className={styles.title } href="/">
-         teshX
-       </a>
-       <div className={styles.menu}>
-         <img
-           className={styles.menuBtn}
-           src={
-             menuOpen
-               ? getImageUrl("nav/closeIcon.png")
-               : getImageUrl("nav/menuIcon.png")
-           }
-           alt="menu-button"
-           onClick={() => setMenuOpen(!menuOpen)}
-         />
-         <ul
-           className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
-           onClick={() => setMenuOpen(false)}
-         >
-           <li>
-             <a href="#about">About</a>
-           </li>
-           <li>
-             <a href="#experience">Experience</a>
-           </li>
-           <li>
-             <a href="#projects">Projects</a>
-           </li>
-           <li>
-             <a href="#contact">Contact</a>
-           </li>
-         </ul>
-       </div>
-     </nav>
-   );
+import "../../index.css";
+import { getImageUrl } from "../../utils.js";
+function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <nav className={styles.navbar}>
+      <a className={styles.title} href="/">
+        teshX
+      </a>
+      <div className={styles.menu}>
+        <img
+          className={styles.menuBtn}
+          src={
+            menuOpen
+              ? getImageUrl("nav/closeIcon.png")
+              : getImageUrl("nav/menuIcon.png")
+          }
+          alt="menu-button"
+          onClick={() => setMenuOpen(!menuOpen)}
+        />
+        <ul
+          className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
+          onClick={() => setMenuOpen(false)}
+        >
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#experience">Experience</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
